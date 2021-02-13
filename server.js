e276f54ed077452f024653218bfc2a5382bbc95f
WarhamsterBoo <warhamsterboo@gmail.com>
2021-02-13 17:48:51 +0300

    expose default prometheus metrics

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const promClient = require('prom-client');
+
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || 'localhost';
 
@@ -9,6 +11,16 @@ app.get('/', (_, res) => {
 })
 
 app.use(require('./routes'));
+
+promClient.collectDefaultMetrics();
+app.get('/metrics', async (_, res) => {
+	try {
+		res.set('Content-Type', promClient.register.contentType);
+		res.end(await promClient.register.metrics());
+	} catch (ex) {
+		res.status(500).end(ex);
+	}
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://${HOST}:${PORT}`)
