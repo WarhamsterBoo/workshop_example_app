5a41e5d6f9093a2bcb403361a935a5eae6b571f2
WarhamsterBoo <warhamsterboo@gmail.com>
2021-02-13 14:02:53 +0300

    add more endpoints

--- routes/weatherforecast.js
+++ routes/weatherforecast.js
@@ -1,11 +1,53 @@
 var router = require('express').Router();
 
-router.get('/:city/today', function (req, res) {
+router.get('/:city/today', (req, res) => {
+    const today = new Date();
     res.json({
-        date: new Date(),
         city: req.params.city,
-        temperature: -20
+        forecast: [{
+            date: today,
+            city: req.params.city,
+            temperature: -20
+        }]
     });
 });
+
+router.get('/:city/tomorrow', (req, res) => {
+    const today = new Date();
+    res.json({
+        city: req.params.city,
+        forecast: [{
+            date: addDays(today, 1),
+            city: req.params.city,
+            temperature: -20
+        }]
+    });
+});
+
+router.get('/:city/next3days', (req, res) => {
+    const today = new Date();
+    res.json({
+        city: req.params.city,
+        forecast: [{
+            date: addDays(today, 1),
+            city: req.params.city,
+            temperature: -20
+        }, {
+            date: addDays(today, 2),
+            city: req.params.city,
+            temperature: -20
+        }, {
+            date: addDays(today, 3),
+            city: req.params.city,
+            temperature: -20
+        }]
+    });
+});
+
+function addDays(date, days) {
+    const resDate = new Date();
+    resDate.setDate(date.getDate() + days);
+    return resDate;
+}
 
 module.exports = router;
