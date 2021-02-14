cc7f947617460f8c2b64523dc5045db7bffbad32
WarhamsterBoo <warhamsterboo@gmail.com>
2021-02-14 19:00:29 +0300

    load can call multiple nodes

--- tools/load.js
+++ tools/load.js
@@ -2,18 +2,19 @@ const fetch = require('node-fetch');
 const { random } = require('../utils');
 
 const HOST = 'localhost';
-const PORT = 5000;
+const PORT_0 = 5000;
+const PORT_1 = 5001;
 const cities = ['Moscow', 'Paris', 'Beijing', 'Washington'];
 const periods = ['today', 'tomorrow', 'next3days'];
 let currentRequests = 0;
 
 const waitFor = ms => new Promise(res => setTimeout(res, ms));
-const randomDelay = () => random(50, 500);
+const randomDelay = () => random(50, 250);
 const randomCity = () => cities[random(0, cities.length)];
 const randomPeriod = () => periods[random(0, periods.length)];
 
-const fetchEndpoint = async () => {
-    const URL = `http://${HOST}:${PORT}/weatherforecast/${randomCity()}/${randomPeriod()}`;
+const fetchEndpoint = async (host, port) => {
+    const URL = `http://${host}:${port}/weatherforecast/${randomCity()}/${randomPeriod()}`;
 
     await fetch(URL);
     currentRequests++;
@@ -23,9 +24,8 @@ const fetchEndpoint = async () => {
     }
 
     await waitFor(randomDelay());
-    await fetchEndpoint();
+    await fetchEndpoint(host, port);
 };
 
-fetchEndpoint();
-fetchEndpoint();
-fetchEndpoint();
+fetchEndpoint(HOST, PORT_0);
+fetchEndpoint(HOST, PORT_1);
