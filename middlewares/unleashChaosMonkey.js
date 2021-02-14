1564c72947614f0557b5d87528ea66a8b9b1a560
WarhamsterBoo <warhamsterboo@gmail.com>
2021-02-14 17:51:40 +0300

    add 400 error for chaos monkeys

--- middlewares/unleashChaosMonkey.js
+++ middlewares/unleashChaosMonkey.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { random } = require('../utils');
 
-const modes = ['none', 'random500'];
+const modes = ['none', 'random500', 'random400'];
 let currentMode = 'none';
 
 const selectMode = (mode) => currentMode = mode;
@@ -9,12 +9,22 @@ const selectMode = (mode) => currentMode = mode;
 const availableModes = () => modes;
 
 router.use((_, __, next) => {
-    if (currentMode != 'none') {
-        if (random(0, 2) == 0) {
-            throw new Error("Chaos monkey said that you shall not pass!");
-        }
+    switch (currentMode) {
+        case 'random500':
+            if (random(0, 2) == 0) {
+                throw new Error("Chaos monkey says that you shall not pass!");
+            }
+            break;
+        case 'random400':
+            if (random(0, 2) == 0) {
+                const error = new Error("Chaos monkey thinks that you said something silly!");
+                error.statusCode = 400;
+                return next(error);
+            }
+            break;
+        default:
+            break;
     }
-
     next();
 });
 
