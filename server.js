3cc316d88b50a42cd23faeaf4af1edc6f7e4d822
WarhamsterBoo <warhamsterboo@gmail.com>
2021-02-14 16:27:08 +0300

    use express-prometheus-middleware

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const promMid = require('express-prometheus-middleware');
 const logger = require('./logger');
 
 const PORT = process.env.PORT || 5000;
@@ -6,7 +7,12 @@ const HOST = process.env.HOST || 'localhost';
 
 const app = express();
 
-app.use(require('./middlewares/performance'));
+app.use(promMid({
+    metricsPath: '/metrics',
+    collectDefaultMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+    prefix: 'workshop_example_app'
+}));
 app.use('/weatherforecast', require('./middlewares/unleashChaosMonkey').router);
 app.use(require('./routes'));
 
