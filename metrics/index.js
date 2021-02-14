9ea673a64fe45eeb59bc4922887982541acb1aad
WarhamsterBoo <warhamsterboo@gmail.com>
2021-02-14 15:22:41 +0300

    do not get prome metrics for /metrics endpoint

--- metrics/index.js
+++ metrics/index.js
@@ -13,18 +13,22 @@ const httpRequestDurationMicroseconds = new promClient.Histogram({
 
 router.use((req, res, next) => {
     const route = url.parse(req.url).pathname;
-    const end = httpRequestDurationMicroseconds.startTimer();
-    next();
-    end({ route, code: res.statusCode, method: req.method });
+    if (route == '/metrics') {
+        next();
+    } else {
+        const end = httpRequestDurationMicroseconds.startTimer();
+        next();
+        end({ route, code: res.statusCode, method: req.method });
+    }
 });
 
 router.get('/metrics', async (_, res) => {
-	try {
-		res.set('Content-Type', promClient.register.contentType);
-		res.end(await promClient.register.metrics());
-	} catch (ex) {
-		res.status(500).end(ex);
-	}
+    try {
+        res.set('Content-Type', promClient.register.contentType);
+        res.end(await promClient.register.metrics());
+    } catch (ex) {
+        res.status(500).end(ex);
+    }
 });
 
 module.exports = router;
