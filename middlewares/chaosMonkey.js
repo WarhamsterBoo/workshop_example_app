9c019ee8474b09e9a2fe7ea19c09402c47e1a179
WarhamsterBoo <warhamsterboo@gmail.com>
2021-02-17 22:08:29 +0300

    refactoring

--- middlewares/chaosMonkey.js
+++ middlewares/chaosMonkey.js
@@ -2,13 +2,12 @@ const router = require('express').Router();
 const { random, waitFor } = require('../utils');
 const logger = require('../logger');
 
-const modes = ['none', 'random500', 'random400', 'randomDelay', 'randomErrors'];
+const modes = ['none', 'random500', 'random400', 'randomDelay', 'randomError'];
 const defaultDelays = [0, 0.1, 0.5, 1, 2, 3, 5];
 let availableDelays = defaultDelays;
 let currentMode = 'none';
 
 const selectMode = (mode) => currentMode = mode;
-
 const selectAvailableDelays = (delays) => {
     try {
         availableDelays = delays.split(",");
@@ -16,38 +15,36 @@ const selectAvailableDelays = (delays) => {
         availableDelays = defaultDelays;
     }
 }
-
 const availableModes = () => modes;
+const tossACoin = () => random(0, 2) === 0;
 
 router.use(async (_, __, next) => {
-    switch (currentMode) {
-        case 'random500':
-            if (random(0, 2) === 0) {
+    if (tossACoin()) {
+        switch (currentMode) {
+            case 'random500':
                 const error = new Error("Chaos monkey says that you shall not pass!");
                 error.statusCode = 500;
                 return next(error);
-            }
-            break;
-        case 'random400':
-            if (random(0, 2) === 0) {
+            case 'random400':
                 const error = new Error("Chaos monkey thinks that you said something silly!");
                 error.statusCode = 400;
                 return next(error);
-            }
-            break;
-        case 'randomDelay':
-            if (random(0, 2) === 0) {
+            case 'randomDelay':
+                logger.log({
+                    level: "warning",
+                    message: `Chaos monkey assumes that your app is slooooow!`
+                });
                 await waitFor(availableDelays[random(0, availableDelays.length)] * 1000);
-            }
-            break;
-        case 'randomErrors':
-            if (random(0, 2) === 0) {
+                return next();
+            case 'randomError':
                 logger.log({
                     level: "error",
                     message: `Chaos monkey insists that something went wrong!`
                 });
-            }
+                return next();
+        }
     }
+
     next();
 });
 
